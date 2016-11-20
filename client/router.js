b101a4d9a0324c648df5a4df582adf058f7cb9ff
Pavneet Kapoor <pavi.kapoor@wishabi.com>
2016-11-19 18:32:32 -0600

    Blaze route config for BoardScribe

---
+++ client/router.js
@@ -0,0 +1,14 @@
+Router.route('/', {
+    name: 'register',
+    template: 'register'
+});
+
+Router.route('/home', {
+    name: 'homePage',
+    template: 'homePage'
+});
+
+Router.route('/login', {
+    name: 'login',
+    template: 'login'
+});
